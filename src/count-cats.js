9a0d6159ec1559150be89e654668d31f301a333a
Gleb <actimel091@gmail.com>
2020-09-27 15:32:13 +0300

    refactor: update count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,3 @@
 module.exports = function countCats(matrix) {
-  return matrix.reduce(
-    (prev, cur) => prev + cur.filter((e) => e === '^^').length,
-    0
-  )
+  return matrix.reduce((a, b) => a + b.filter((cat) => cat === '^^').length, 0)
 }
